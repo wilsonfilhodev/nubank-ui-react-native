543db75b216a2b1d8a3a98b2f59b946dbd5f7457
Wilson Filho <will.filho27@gmail.com>
2019-05-09 20:32:53 -0300

    commit initial

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+      <Tabs />
+    </Container>
+  );
+}
